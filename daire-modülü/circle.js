9f38fea60f4dcc10ab8ac543e79fd6c2e80ba84b
osmanyagar <osmannuriyagar@gmail.com>
2021-09-05 01:10:12 +0300

    added circle as folder

---
+++ daire-modülü/circle.js
@@ -0,0 +1,13 @@
+const circleArea = (r) =>{
+    console.log(Math.PI *(r**2))
+}
+
+const circleCircumference  = (r) =>{
+    console.log(Math.PI *r*2)
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
+
